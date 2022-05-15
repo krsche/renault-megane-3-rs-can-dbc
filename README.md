c212620959ba8936b6ad47d0d9eb73bcdf6133fa
Fabian Kirschner <fabian.kirschner@gmail.com>
2022-05-15 17:55:26 +0200

    feat: initial commit of dbc-gen w/ whl-spd signals

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
-# Repository Template-Basic
+# Renault Megane 3 RS - CAN Bus reverse engineerung
 
-Can be used with `gh repo create trackdocs/some-repo --private --disable-wiki --template trackdocs/template-basic`
+__Gettings Started:__
+```bash
+# Install dependencies
+python3 -m pip install -r requirements.txt
+
+# Generate dbc
+python3 generate-dbc.py
+
+# Install can-utils on linux
+sudo apt install can-utils
+
+# Dump Can Messages (after connecting usb-can dongle)
+
+```
+
+# Peak PCAN - SocketCan Cheatsheet
+
+```bash
+# Download linux drivers from website
+
+# in order to use socket-can, driver needs to be build in netdev mode
+make clean
+make -j12 NET=NETDEV_SUPPORT
+make install
+
+# check if kernel module is loaded / load it
+lsmod | grep pcan
+sudo modprobe pcan
+
+# bring can interface up
+ip link set can0 type can help
+ip link set can0 type can bitrate 500000 listen-only on
+ip -details link show can0
+sudo ip link set can0 up
+
+# start socketcan - candump w/ decoding via dbc
+candump can0 | cantools decode --single-line renault-megane-3-rs.dbc
+candump can0 | cantools monitor --single-line renault-megane-3-rs.dbc
+
+# plot Wheel_Speed signals from saved measurement, with excluding first n lines
+cat dump-example.txt | tail -n +3000 | cantools plot renault-megane-3-rs.dbc 'Wheel_Speed_*'
+```
---
+++ generate-dbc.py
@@ -0,0 +1,55 @@
+import cantools
+
+# For Bit Numbering in a message w/ big-endian / little-endian, see:
+# https://cantools.readthedocs.io/en/latest/#cantools.database.can.Signal
+
+messages = [
+    cantools.db.Message(0x29C, "Wheel_Speed_Rear", 8,
+        signals=[
+            cantools.db.Signal("Wheel_Speed_RL",
+                                start=7,
+                                length=16,
+                                byte_order='big_endian',
+                                is_signed=False,
+                                offset=0,
+                                scale=0.01,
+                                minimum=0,
+                                unit="km/h"),
+            cantools.db.Signal("Wheel_Speed_RR",
+                                start=23,
+                                length=16,
+                                byte_order='big_endian',
+                                is_signed=False,
+                                offset=0,
+                                scale=0.01,
+                                minimum=0,
+                                unit="km/h")
+        ]),
+
+    cantools.db.Message(0x29A, "Wheel_Speed_Front", 7,
+        signals=[
+            cantools.db.Signal("Wheel_Speed_FR",
+                                start=7,
+                                length=16,
+                                byte_order='big_endian',
+                                is_signed=False,
+                                offset=0,
+                                scale=0.01,
+                                minimum=0,
+                                unit="km/h"),
+            cantools.db.Signal("Wheel_Speed_FL",
+                                start=23,
+                                length=16,
+                                byte_order='big_endian',
+                                is_signed=False,
+                                offset=0,
+                                scale=0.01,
+                                minimum=0,
+                                unit="km/h")
+        ])
+]
+
+bus = cantools.db.database.Bus("OBD2-Port", baudrate=500000)
+db = cantools.db.Database(messages, None, [bus])
+
+cantools.db.dump_file(db, "renault-megane-3-rs.dbc", 'dbc')
